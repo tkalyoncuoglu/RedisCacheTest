d7b28ad8c4dab9f68f284e2746aebd5368051689
29534411212 <tkalyoncuoglu@gmail.com>
2024-12-26 00:35:43 +0300

    refactored

--- RedisCacheTest/Program.cs
+++ RedisCacheTest/Program.cs
@@ -1,4 +1,5 @@
 using System.Text.Json;
+using MessagePack;
 using RedisCacheTest;
 using StackExchange.Redis;
 
@@ -12,6 +13,12 @@ var fiveMillionObjects = new List<O>();
 for (int i = 0; i < 5_000_000; i++)
 {
     fiveMillionObjects.Add(new O(Guid.NewGuid()));
+}
+
+var fiveMillionObjects2 = new List<Data>();
+for (int i = 0; i < 5_000_000; i++)
+{
+    fiveMillionObjects2.Add(new Data { Id = Guid.NewGuid() });
 }
 
 app.MapGet("/normal-cache", async (IConnectionMultiplexer redis) =>
@@ -30,13 +37,52 @@ app.MapGet("/normal-cache", async (IConnectionMultiplexer redis) =>
     return fiveMillionObjects;
 });
 
-// app.UseMiddleware<ResponseCacheMiddleware>();
-//
-// app.MapGet("/middleware-cache", () =>
-// {
-//     return fiveMillionObjects;
-// });
+
+app.MapGet("/normal-cache2", async (IConnectionMultiplexer redis) =>
+{
+    var cachedResponse = await redis.GetDatabase().StringGetAsync("normal-cache2");
+    if (cachedResponse.HasValue)
+    {
+        return Serializer.DeserializeFromBinary<List<Data>>(cachedResponse);
+    }
+
+    var json = Serializer.SerializeToBinary(fiveMillionObjects2);
+
+    var db = redis.GetDatabase();
+    await db.StringSetAsync("normal-cache2", json, TimeSpan.FromMinutes(10));
+
+    return fiveMillionObjects2;
+});
+
+ app.UseMiddleware<ResponseCacheMiddleware>();
+
+ app.MapGet("/middleware-cache", () =>
+ {
+     return fiveMillionObjects;
+ });
 
 app.Run();
 
 record O(Guid Id);
+
+
+[MessagePackObject] 
+public class Data
+{
+    [Key(0)]
+    public Guid Id { get; set; }
+}
+
+public class Serializer
+{
+    // Binary JSON (MessagePack)
+    public static byte[] SerializeToBinary<T>(T obj)
+    {
+        return MessagePackSerializer.Serialize(obj);
+    }
+
+    public static T DeserializeFromBinary<T>(byte[] data)
+    {
+        return MessagePackSerializer.Deserialize<T>(data);
+    }
+}
